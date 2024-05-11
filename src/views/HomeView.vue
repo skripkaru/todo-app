58dd310dcb4320c3aadd40cc63dcdba11f7cfbcf
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2024-05-11 12:20:59 +0300

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Todo App</h1>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 16px;
+}
+</style>
